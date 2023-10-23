bbd1dc48b30d755316ea728cc93d5241b1dc8f51
usmanazafar <usmanzafar@godatadriven.com>
2023-10-23 15:23:05 +0200

    remove unnecessary prints

--- gdf_workshop/ml.py
+++ gdf_workshop/ml.py
@@ -24,5 +24,4 @@ def train_model(fields, training_data):
 def evaluate_model(model, input_data):
     valid_preds = model.transform(input_data)
     acc = BinaryClassificationEvaluator().evaluate(valid_preds)
-    valid_preds.show(truncate=False)
     print(acc)
